25afb57a73bd0f15ce09a632e4fbfb238e96fe75
Rachman-Akbar <akbarfahlevy39@gmail.com>
2026-04-23 13:17:56 +0700

    jsha

--- app/Domains/Catalog/Presentation/Http/Resources/BannerResource.php
+++ app/Domains/Catalog/Presentation/Http/Resources/BannerResource.php
@@ -9,14 +9,14 @@ class BannerResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'id' => $this->id(),
-            'title' => $this->title(),
-            'subtitle' => method_exists($this->resource, 'subtitle') ? $this->subtitle() : null,
-            'image_url' => $this->imageUrl(),
-            'mobile_image_url' => method_exists($this->resource, 'mobileImageUrl') ? $this->mobileImageUrl() : null,
-            'link_type' => method_exists($this->resource, 'linkType') ? $this->linkType() : null,
-            'link_url' => $this->linkUrl(),
-            'is_active' => $this->isActive(),
+            'id' => $this->id,
+            'title' => $this->title,
+            'subtitle' => null,
+            'image_url' => $this->imageUrl,
+            'mobile_image_url' => null,
+            'link_type' => null,
+            'link_url' => $this->linkUrl,
+            'is_active' => $this->isActive,
         ];
     }
 }
